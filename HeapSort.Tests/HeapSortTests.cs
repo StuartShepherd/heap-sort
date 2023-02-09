456bda4cc64ad22fb7fcf44ac62ac38730099df9
Stuart Shepherd <stuart@createstudios.com.au>
2023-02-09 16:52:42 +1100

    Fixed the tests class.

--- HeapSort.Tests/HeapSortTests.cs
+++ HeapSort.Tests/HeapSortTests.cs
@@ -3,18 +3,14 @@ namespace HeapSort.Tests;
 [TestClass]
 public class HeapSortTests
 {
-    [TestClass]
-    public class MergeSortTests
+    [TestMethod]
+    [DataRow(new int[] { 73, 57, 49, 99, 133, 20, 1 }, new int[] { 1, 20, 49, 57, 73, 99, 133 })]
+    public void Sorts_Array_ReturnsSortedArray(int[] x, int[] expected)
     {
-        [TestMethod]
-        [DataRow(new int[] { 73, 57, 49, 99, 133, 20, 1 }, new int[] { 1, 20, 49, 57, 73, 99, 133 })]
-        public void Sorts_Array_ReturnsSortedArray(int[] x, int[] expected)
-        {
-            var heapSort = new HeapSort();
-            var actual = heapSort.Sort(x);
+        var heapSort = new HeapSort();
+        var actual = heapSort.Sort(x);
 
-            Assert.IsNotNull(actual);
-            CollectionAssert.AreEqual(actual, expected);
-        }
+        Assert.IsNotNull(actual);
+        CollectionAssert.AreEqual(actual, expected);
     }
 }
